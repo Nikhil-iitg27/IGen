263a37c8093edae7c579323e2b49427c33047127
Nikhil-iitg27 <bhupathinikhil2006@gmail.com>
2025-06-03 19:33:09 +0530

    made changes for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import style from './components/assets/App.module.css'
 import Prompt from './components/Prompt.jsx'
 function App() {
-
+  console.log('Backend URL:', import.meta.env.VITE_BACKEND_URL);
   return (
     <div className={style.appContainer}>
       <div className={style.app}>
